a323ccf542305d1b494723570e9b1f25c2976088
guridaek <iue16@naver.com>
2022-12-12 14:23:17 +0900

    feat: 정답 숫자 생성 기능 구현

---
+++ src/BaseballGame.js
@@ -0,0 +1,19 @@
+const { Random } = require("@woowacourse/mission-utils");
+
+class BaseballGame {
+  #answerNumber;
+
+  constructor() {
+    this.resetAnswerNumber();
+  }
+
+  resetAnswerNumber() {
+    let numbers = new Set();
+    while (numbers.size < 3) {
+      numbers.add(Random.pickNumberInRange(1, 9));
+    }
+    this.#answerNumber = Array.from(numbers);
+  }
+}
+
+module.exports = BaseballGame;
